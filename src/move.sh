e9723a564401d8af0ee1c315af04c7cee2094772
Abhijit Roy <dalvath3700@gmail.com>
2019-03-12 01:47:53 +0530

    hello, mv added

---
+++ src/move.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+##########################################################################
+# move file from one dir to another
+##########################################################################
+
+if [[ -d "dummy" ]]; then
+	echo "file is there"
+else
+	echo "Folder is not there"
+fi

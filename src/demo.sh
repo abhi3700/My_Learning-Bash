92d43136c603801de49e9d4d792a2a04c2f8ad4a
Abhijit Roy <dalvath3700@gmail.com>
2019-03-18 01:50:18 +0530

    alternative in demo script added

--- src/demo.sh
+++ src/demo.sh
@@ -4,7 +4,9 @@
 # Here, the shell script can be executed by clicking the [shortcut link - `"demo - Shortcut.lnk"`.
 # Change the `Target` param in the properties of the shortcut link file.
 # Set Target as = <git-shell-file> --login -i <shell-file> 
-# E.g. - F:\Softwares\Git\bin\sh.exe --login -i "F:\Coding\Github_repos\My_Learning_Bash\src\demo.sh"
+# E.g. (2 ways) 
+# - F:\Softwares\Git\bin\sh.exe --login -i "F:\Coding\Github_repos\My_Learning_Bash\src\demo.sh"
+# - F:\Softwares\Git\bin\bash.exe --login -i "F:\Coding\Github_repos\My_Learning_Bash\src\demo.sh" [RECOMMENDED w.r.t Unix]
 
 # NOTE: Don't use `git-bash.exe` in place of `sh.exe`
 ##########################################################################

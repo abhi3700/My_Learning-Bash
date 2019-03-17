d762fb9c26097eae6ef53f3e5d8d7b415da96c68
Abhijit Roy <dalvath3700@gmail.com>
2019-03-18 01:40:18 +0530

    demo shell script modified

--- src/demo.sh
+++ src/demo.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
+##########################################################################
 # Ask the user for their name
+# Here, the shell script can be executed by clicking the [shortcut link - `"demo - Shortcut.lnk"`.
+# Change the `Target` param in the properties of the shortcut link file.
+# Set Target as = <git-shell-file> --login -i <shell-file> 
+# E.g. - F:\Softwares\Git\bin\sh.exe --login -i "F:\Coding\Github_repos\My_Learning_Bash\src\demo.sh"
+
+# NOTE: Don't use `git-bash.exe` in place of `sh.exe`
+##########################################################################
+
 echo Hello, who am I talking to?
 read varname
 echo It\'s nice to meet you $varname

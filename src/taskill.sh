adf56a9b0d2e6b6d3d5cc20024c36c83bb3cf8bc
Abhijit Roy <dalvath3700@gmail.com>
2019-12-07 13:23:20 +0530

    taskkill sh added

---
+++ src/taskill.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+##########################################################################
+# This kills the task. E.g. - excel, word, notepad
+# first open the excel & then run this.
+##########################################################################
+tskill excel
